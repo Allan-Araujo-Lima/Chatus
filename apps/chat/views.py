9a6534f531f598eedfc8393e5b570b94537435a1
Allan Araujo Lima <62760029+Allan-Araujo-Lima@users.noreply.github.com>
2024-05-02 23:40:22 -0300

    doideira

--- apps/chat/views.py
+++ apps/chat/views.py
@@ -3,7 +3,7 @@ from .models import Message, Room
 from django.views.generic.detail import DetailView
 
 def home(request):
-    rooms = Room.objects.all()
+    rooms = Room.objects.all().order_by('created_at')
     return render(request, 'chat/home.html', {
         'rooms': rooms,
     })
@@ -15,4 +15,3 @@ class RoomDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
-        
--- static/js/chatus.js
+++ static/js/chatus.js
@@ -4,6 +4,5 @@ const getMessages = async (room_id) => {
     const reponse = await fetch(`/${room_id}`);
     const html = await reponse.text();
     $chatMessages.innerHTML = html;
+    setRoomActive(room_id);
 };
-
-getMessages(8);
