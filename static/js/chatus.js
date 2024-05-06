51294c375b9884f00c932995683917db546c962b
Allan Araujo Lima <62760029+Allan-Araujo-Lima@users.noreply.github.com>
2024-05-06 20:41:59 -0300

    commit doido

--- static/js/chatus.js
+++ static/js/chatus.js
@@ -1,8 +1,27 @@
 const $chatMessages = document.querySelector(".messages");
 
+const setRoomActive = (room_id) => {
+    document
+        .querySelectorAll(".list-rooms li")
+        .forEach((el) => el.classList.remove("active"));
+
+    document.querySelector(`#room-${room_id}`).classList.add("active");
+};
+
 const getMessages = async (room_id) => {
-    console.log("oi")
     const response = await fetch(`/${room_id}`);
     const html = await response.text();
     $chatMessages.innerHTML = html;
+    setRoomActive(room_id);
+};
+
+function mouseOver(room_id) {
+    document.getElementById(room_id).style.border = '15px solid green';
+    console.log('oi');
+};
+
+function mouseOut(room_id) {
+    document.querySelectorAll(".list-rooms li")
+    document.getElementById(room_id).style.border = '5px solid black';
+    console.log('oi');
 };
